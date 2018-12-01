c50c9ea66495382ac9e82cd33b8d8610defa1d47
Daniel R <drothchild@berkeley.edu>
2018-12-01 10:12:53 -0800

    Moved tests in csh.py to test.py

--- csh.py
+++ csh.py
@@ -57,27 +57,3 @@ class CSH(object):
     def l2estimate(self):
         # l2 norm esimation from the sketch
         return np.sqrt(np.median(np.sum(self.tables[0]**2, 1)))
-
-if __name__=="__main__":
-    print "running a toy example - sketch size 3x20 with 5 levels in hierarchy, dictionary size 10**6" 
-    c= 20; r=3; d=5; n=10**6  
-   
-    print "sketch csh1 fed with 10^4 updates with item 43 appearing 500 times and everybody else once"
-    csh1 = CSH(c, r, d, n) 
-    for i in range(10**4):
-        csh1.update(i,1)
-        if i%20 ==0:  
-            csh1.update(43,1) 
-    
-    print "sketch csh2 fed with 10^4 updates with item 42 appearing 1000 times and everybody else once"
-    csh2 = CSH(c, r, d, n) 
-    for i in range(10**4):
-        csh2.update(i,1)
-        if i%10 ==0:  
-            csh2.update(42,1)
-    # print out l2 norms for both streams    
-    print "l2 norm for stream1: ", csh1.l2estimate(), "; l2 norm for stream2: ", csh2.l2estimate()
-    csh2.merge(csh1)
-    print "l2 norm from merged sketches", csh2.l2estimate()
-    hhs = csh2.findHH(thr= 0.2 * csh2.l2estimate(), prefix=0, h=4)
-    print "coordinates with values > 0.2 l2 norm: ", hhs 
