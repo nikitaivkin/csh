ab2006ac01152f46b67e5e62df8cbf202bdca07c
Daniel R <drothchild@berkeley.edu>
2019-03-22 14:39:42 -0700

    Packagize

---
+++ csvec/__init__.py
@@ -0,0 +1,1 @@
+from .csvec import CSVec
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='csvec',
+      version='0.0.1',
+      description='Count Sketch Vector',
+      url='https://github.com/nikitaivkin/csh',
+      author='Nikita Ivkin',
+      author_email='nikitasemail@nikita.com',
+      license='MIT',
+      packages=['csvec'],
+      zip_safe=False)
